bff70b93e3d0a6951d0d812a5ba36db93d1c06c0
aleksander <er.pawlus@gmail.com>
2026-03-10 10:12:16 +0000

    updated app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return {"message": "Hello from CI/CD pipeline"}
+    return {"message": "Hello from CI/CD pipeline. Updated by a pipeline triggered by git push"}
 
 if __name__   == "__main__":
     app.run(host="0.0.0.0", port=5000)
